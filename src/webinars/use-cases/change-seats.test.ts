e42c62e59b819923c279f78ad7e7f9e792289216
maya aityahia <maya.aityahia.etu@univ-lille.fr>
2026-02-03 16:10:05 +0100

    test(change-seats) : ajout du test unitaire du happy path

--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -1,13 +1,42 @@
 // Tests unitaires
 
+import { testUser } from "src/users/tests/user-seeds";
+import { InMemoryWebinarRepository } from "src/webinars/adapters/webinar-repository.in-memory";
+import { ChangeSeats } from "./change-seats";
+import { Webinar } from "../entities/webinar.entity";
 
 describe('Feature : Change seats', () => {
   // Initialisation de nos tests, boilerplates...
+  let webinarRepository: InMemoryWebinarRepository;
+  let useCase: ChangeSeats;
+
+  const webinar = new Webinar({
+    id: 'webinar-id',
+    organizerId: testUser.alice.props.id,
+    title: 'Webinar title',
+    startDate: new Date('2024-01-01T00:00:00Z'),
+    endDate: new Date('2024-01-01T01:00:00Z'),
+    seats: 100,
+  });
+
+  beforeEach(() => {
+    webinarRepository = new InMemoryWebinarRepository([webinar]);
+    useCase = new ChangeSeats(webinarRepository);
+});
+
   describe('Scenario: Happy path', () => {
     // Code commun à notre scénario : payload...
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 200,
+    };
     it('should change the number of seats for a webinar', async () => {
      // Vérification de la règle métier, condition testée...
-      throw new Error('Not implemented');
+      await useCase.execute(payload);
+      // ASSERT
+      const updatedWebinar = await webinarRepository.findById('webinar-id');
+      expect(updatedWebinar?.props.seats).toEqual(200);
     });
   });
 });
